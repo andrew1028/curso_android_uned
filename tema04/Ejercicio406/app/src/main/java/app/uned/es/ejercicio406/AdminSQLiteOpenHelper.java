98eeee04dbcb30603ad48f715a54c0e9a562de43
Fran <fran.lucena@gmail.com>
2015-01-30 19:42:25 +0100

    [FIX] Sintaxis

--- tema04/Ejercicio406/app/src/main/java/app/uned/es/ejercicio406/AdminSQLiteOpenHelper.java
+++ tema04/Ejercicio406/app/src/main/java/app/uned/es/ejercicio406/AdminSQLiteOpenHelper.java
@@ -13,7 +13,7 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     private static CursorFactory factory = null;
 
 
-    public AdminSQLiteOpenHelper(Context context, String name, CursorFactory factory, int version) {
+    public AdminSQLiteOpenHelper(Context context) {
         super(context, name, factory, version);
     }
 
@@ -23,9 +23,9 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
 
         db.execSQL( "CREATE TABLE DEPARTAMENTO(" +
                 " _id INTEGER PRIMARY KEY," +
-                " nombre TEXT NOT NULL " );
+                " nombre TEXT NOT NULL)" );
 
-        db.execSQL( "CREATE UNIQUE INDEX nombre ON DEPARTAMENTO(nombre ASC)" );
+        db.execSQL( "CREATE UNIQUE INDEX nombreDep ON DEPARTAMENTO(nombre ASC)" );
 
         Log.i(this.getClass().toString(), "Tabla DEPARTAMENTO creada");
 
@@ -34,36 +34,35 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO DEPARTAMENTO(_id, nombre) VALUES(3,'Marketing')");
         db.execSQL("INSERT INTO DEPARTAMENTO(_id, nombre) VALUES(4,'Recursos Humanos')");
         
-        Log.i(this.getClass().toString(), "Datos iniciales DEPARTAMENTO insertados");
+        Log.i(this.getClass().toString(), "Datos iniciales de DEPARTAMENTO insertados");
 
-        db.execSQL( "CREATE TABLE EMPLEADOS(" +
+        db.execSQL( "CREATE TABLE EMPLEADO(" +
                 " _id INTEGER PRIMARY KEY," +
-                " nombre TEXT NOT NULL " +
-                " salario INTEGER NOT NULL " +
-                " iddepartamento INTEGER NOT NULL ");
+                " nombre TEXT NOT NULL," +
+                " salario INTEGER NOT NULL," +
+                " iddepartamento INTEGER NOT NULL)");
 
-        db.execSQL( "CREATE UNIQUE INDEX nombre ON EMPLEADOS (nombre ASC)" );
+        db.execSQL( "CREATE UNIQUE INDEX nombreEmp ON EMPLEADO(nombre ASC)" );
 
-        Log.i(this.getClass().toString(), "Tabla EMPLEADOS creada");
+        Log.i(this.getClass().toString(), "Tabla EMPLEADO creada");
 
-        db.execSQL("INSERT INTO EMPLEADOS(_id, nombre, salario, iddepartamento) VALUES(1,'Alberto Puertas', 110000, 1)");
-        db.execSQL("INSERT INTO EMPLEADOS(_id, nombre, salario, iddepartamento) VALUES(2,'Carlos Moreno', 120000, 2)");
-        db.execSQL("INSERT INTO EMPLEADOS(_id, nombre, salario, iddepartamento) VALUES(3,'María Lucena', 130000, 3)");
-        db.execSQL("INSERT INTO EMPLEADOS(_id, nombre, salario, iddepartamento) VALUES(4,'Esteban Pérez', 140000, 4)");
-        db.execSQL("INSERT INTO EMPLEADOS(_id, nombre, salario, iddepartamento) VALUES(5,'Jose Gómez', 40000, 3)");
-        db.execSQL("INSERT INTO EMPLEADOS(_id, nombre, salario, iddepartamento) VALUES(6,'Pedro Salmerón', 3000, 2)");
+        db.execSQL("INSERT INTO EMPLEADO(_id, nombre, salario, iddepartamento) VALUES(1,'Alberto Puertas', 110000, 1)");
+        db.execSQL("INSERT INTO EMPLEADO(_id, nombre, salario, iddepartamento) VALUES(2,'Carlos Moreno', 120000, 2)");
+        db.execSQL("INSERT INTO EMPLEADO(_id, nombre, salario, iddepartamento) VALUES(3,'María Lucena', 130000, 3)");
+        db.execSQL("INSERT INTO EMPLEADO(_id, nombre, salario, iddepartamento) VALUES(4,'Esteban Pérez', 140000, 4)");
+        db.execSQL("INSERT INTO EMPLEADO(_id, nombre, salario, iddepartamento) VALUES(5,'Jose Gómez', 40000, 3)");
+        db.execSQL("INSERT INTO EMPLEADO(_id, nombre, salario, iddepartamento) VALUES(6,'Pedro Salmerón', 3000, 2)");
 
-        Log.i(this.getClass().toString(), "Datos iniciales EMPLEADOS insertados");
-
-
-
-        Log.i(this.getClass().toString(), "Base de datos creada");
+        Log.i(this.getClass().toString(), "Datos iniciales de EMPLEADO insertados");
+        Log.i(this.getClass().toString(), "Base de datos creada.");
         
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXIST DEPARTAMENTOS");
-        db.execSQL("DROP TABLE IF EXIST EMPLEADOS");
+        db.execSQL("DROP TABLE IF EXIST DEPARTAMENTO");
+        Log.i(this.getClass().toString(), "Eliminada la tabla DEPARTAMENTO.");
+        db.execSQL("DROP TABLE IF EXIST EMPLEADO");
+        Log.i(this.getClass().toString(), "Eliminada la tabla EMPLEADO.");
     }
 
 
