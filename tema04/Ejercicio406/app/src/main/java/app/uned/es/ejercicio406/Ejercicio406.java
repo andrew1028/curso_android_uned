1d212a022ada15c377b6564958ed6084dc7e532d
Fran <fran.lucena@gmail.com>
2015-01-31 11:15:33 +0100

    Se cargan los empleados en el Spinner y se muestra un mensaje cuando se selecciona uno de ellos. Imagen con el resultado para Ejercicio406

--- tema04/Ejercicio406/app/src/main/java/app/uned/es/ejercicio406/Ejercicio406.java
+++ tema04/Ejercicio406/app/src/main/java/app/uned/es/ejercicio406/Ejercicio406.java
@@ -1,13 +1,18 @@
 package app.uned.es.ejercicio406;
 
+import android.content.ClipData;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,6 +21,7 @@ import java.util.ArrayList;
 public class Ejercicio406 extends ActionBarActivity {
 
     private ListView lvDep;
+    private Spinner spEmp;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,8 @@ public class Ejercicio406 extends ActionBarActivity {
         setContentView(R.layout.activity_ejercicio406);
 
         lvDep = (ListView)findViewById(R.id.lvDepartamento);
-        
+        spEmp = (Spinner)findViewById(R.id.spEmpleados);
+
         AdminSQLiteOpenHelper dbHelper = new AdminSQLiteOpenHelper(getBaseContext());
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         Toast.makeText(getBaseContext(), "Base de datos preparada.", Toast.LENGTH_SHORT).show();
@@ -35,7 +42,40 @@ public class Ejercicio406 extends ActionBarActivity {
                 nombres.add(c1.getString(0));
             } while (c1.moveToNext());
         }
+        
         lvDep.setAdapter(arrayAdapter);
+        lvDep.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                String query = "SELECT nombre FROM EMPLEADO WHERE iddepartamento=" + (position+1);
+                spEmp.setAdapter(datosSpinner(query));
+            }
+        });
+        
+        spEmp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String nombre = spEmp.getItemAtPosition(position).toString();
+                Toast.makeText(getBaseContext(), "Has seleccionado a " + nombre + ".", Toast.LENGTH_SHORT).show();
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+    
+    public ArrayAdapter<String> datosSpinner (String query){
+        AdminSQLiteOpenHelper dbHelper = new AdminSQLiteOpenHelper(getBaseContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor c2 = db.rawQuery(query, null);
+        ArrayList<String> nombres = new ArrayList<String>();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item, nombres);
+        if (c2.moveToFirst()) {
+            do {
+                nombres.add(c2.getString(0));
+            } while (c2.moveToNext());
+        }
+        
+        return arrayAdapter;
     }
 
     @Override
