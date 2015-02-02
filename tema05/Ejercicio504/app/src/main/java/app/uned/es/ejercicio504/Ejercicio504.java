d0e3a5d932dd3e7b8fba63e6fd7ba4c856ab423b
Fran <fran.lucena@gmail.com>
2015-02-02 21:01:13 +0100

    Ejercicio 504

--- tema05/Ejercicio504/app/src/main/java/app/uned/es/ejercicio504/Ejercicio504.java
+++ tema05/Ejercicio504/app/src/main/java/app/uned/es/ejercicio504/Ejercicio504.java
@@ -4,14 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.VideoView;
 
 
 public class Ejercicio504 extends ActionBarActivity {
 
+    private VideoView vVideo;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio504);
+        
+        vVideo = (VideoView)findViewById(R.id.vvVideo);
+        vVideo.setVideoPath("mnt/sdcard/movie.mp4");
+        vVideo.start();
     }
 
 
