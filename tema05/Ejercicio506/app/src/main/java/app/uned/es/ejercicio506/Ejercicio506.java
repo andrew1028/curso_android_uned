09d50054f3449d66ccd550aaa68ec7e617ac8083
Fran <fran.lucena@gmail.com>
2015-02-04 17:47:40 +0100

    Ejercicio 506 LocationListener

--- tema05/Ejercicio506/app/src/main/java/app/uned/es/ejercicio506/Ejercicio506.java
+++ tema05/Ejercicio506/app/src/main/java/app/uned/es/ejercicio506/Ejercicio506.java
@@ -1,17 +1,57 @@
 package app.uned.es.ejercicio506;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class Ejercicio506 extends ActionBarActivity {
 
+    private TextView tVLoc;
+    private LocationManager lM;
+    private LocationListener mLocationListener;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio506);
+
+        tVLoc = (TextView)findViewById(R.id.tvLoc);
+        lM = (LocationManager)getSystemService(LOCATION_SERVICE);
+        mLocationListener = new MyLocationListener();
+        lM.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+    }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+            //TODO Auto-generated method stub
+            if (loc != null) {
+                tVLoc.setText("Localización encontrada: \nLatitud: " + loc.getLatitude()
+                        + " \nLongitud: " + loc.getLongitude() + "\nAltitud: " +
+                        loc.getAltitude());
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 
 
