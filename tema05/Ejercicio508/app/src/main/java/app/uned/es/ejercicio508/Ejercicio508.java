98f04e2026651ccb875158e386ce01f2503ba885
Fran <fran.lucena@gmail.com>
2015-02-04 18:36:36 +0100

    Plantilla Ejercicio 508

--- tema05/Ejercicio508/app/src/main/java/app/uned/es/ejercicio508/Ejercicio508.java
+++ tema05/Ejercicio508/app/src/main/java/app/uned/es/ejercicio508/Ejercicio508.java
@@ -1,20 +1,54 @@
 package app.uned.es.ejercicio508;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
 
 
-public class Ejercicio508 extends ActionBarActivity {
+public class Ejercicio508 extends ActionBarActivity implements SensorEventListener {
 
+    private TextView tvEventos;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio508);
+
+        tvEventos = (TextView)findViewById(R.id.tvEventos);
+        SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        List<Sensor> listaSensores = sensorManager.getSensorList(Sensor.TYPE_PROXIMITY);
+        if (!listaSensores.isEmpty()) {
+            Sensor proximitySensor = listaSensores.get(0);
+            sensorManager.registerListener(this, proximitySensor,SensorManager.SENSOR_DELAY_UI);}
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        synchronized (this) {
+            switch(event.sensor.getType()) {
+                case Sensor.TYPE_PROXIMITY:
+                    for (int i=0 ; i<event.values.length ; i++) {
+                        tvEventos.append("Proximidad " + i + ": " + event.values[i] +"\n");
+                    }
+                    break;
+            }
+        }
+    }
+    
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //TODO Auto-generated method stub
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
